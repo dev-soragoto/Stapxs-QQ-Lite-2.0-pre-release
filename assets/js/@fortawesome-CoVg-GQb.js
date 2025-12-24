109d09b150e3112da3481e64cd127baddd42fe95
dev-soragoto <soragoto@soragoto.io>
2025-12-24 00:53:45 +0000

    Deploying to gh-pages from @ dev-soragoto/Stapxs-QQ-Lite-2.0-pre-release@a51cf120e5b381437b7085d51eca804daea1f86d 🚀

--- assets/js/@fortawesome-CoVg-GQb.js
+++ assets/js/@fortawesome-CoVg-GQb.js
@@ -565,4 +565,4 @@ svg:not(:root).svg-inline--fa, svg:not(:host).svg-inline--fa {
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
- */const KP={prefix:"far",iconName:"square",icon:[448,512,[9632,9723,9724,61590],"f0c8","M384 80c8.8 0 16 7.2 16 16l0 320c0 8.8-7.2 16-16 16L64 432c-8.8 0-16-7.2-16-16L48 96c0-8.8 7.2-16 16-16l320 0zM64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32z"]};export{XP as F,V7 as a,by as b,M4 as c,BN as d,KP as e,Ec as f,YP as i,VP as l};
+ */const KP={prefix:"far",iconName:"square",icon:[448,512,[9632,9723,9724,61590],"f0c8","M384 80c8.8 0 16 7.2 16 16l0 320c0 8.8-7.2 16-16 16L64 432c-8.8 0-16-7.2-16-16L48 96c0-8.8 7.2-16 16-16l320 0zM64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32z"]};export{XP as F,V7 as a,by as b,M4 as c,BN as d,dk as e,Ec as f,KP as g,YP as i,VP as l};
